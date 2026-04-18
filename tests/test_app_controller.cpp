d78270b23b69caca83485e412b3b3d88ecf5a14c
Szymon <sz.oleskiewicz@gmail.com>
2026-04-18 09:24:09 +0200

    test(qt-app): add tests
    Added tests for classes:
    - AppController
    - CodeEditor
    - QuantumCircuitView (placeholder)
    - TextDocument
    - TextEditor
    
    - Test for main.cpp - correct app initialization

---
+++ tests/test_app_controller.cpp
@@ -0,0 +1,75 @@
+#include <gtest/gtest.h>
+#include <QSignalSpy>
+#include <QTest>
+#include "qde/qt/app_controller.hpp"
+#include "qde/qt/quantum_circuit_view.hpp"
+#include "qde/qt/text_editor.hpp"
+
+class AppControllerTest : public ::testing::Test {
+protected:
+ void SetUp() override {
+  circuitView = new QuantumCircuitView();
+  textEditor = new TextEditor();
+  controller = new AppController(circuitView, textEditor);
+ }
+
+ void TearDown() override {
+  delete controller;
+  delete textEditor;
+  delete circuitView;
+ }
+
+ QuantumCircuitView* circuitView;
+ TextEditor* textEditor;
+ AppController* controller;
+};
+
+TEST_F(AppControllerTest, ParseNowSuccess) {
+ QSignalSpy successSpy(controller, &AppController::parseSuccess);
+ QSignalSpy errorSpy(controller, &AppController::parseError);
+
+ CodeEditor* innerEditor = textEditor->findChild<CodeEditor*>();
+ ASSERT_NE(innerEditor, nullptr);
+ innerEditor->setPlainText("OPENQASM 3.0;\nqreg q[1];");
+
+ controller->parseNow();
+
+ EXPECT_EQ(successSpy.count(), 1);
+ EXPECT_EQ(errorSpy.count(), 0);
+ EXPECT_NE(controller->circuit(), nullptr);
+}
+
+TEST_F(AppControllerTest, ParseNowError) {
+ QSignalSpy successSpy(controller, &AppController::parseSuccess);
+ QSignalSpy errorSpy(controller, &AppController::parseError);
+
+ CodeEditor* innerEditor = textEditor->findChild<CodeEditor*>();
+ ASSERT_NE(innerEditor, nullptr);
+ innerEditor->setPlainText("INVALID SYNTAX");
+
+ controller->parseNow();
+
+ EXPECT_EQ(successSpy.count(), 0);
+ EXPECT_EQ(errorSpy.count(), 1);
+ EXPECT_EQ(controller->circuit(), nullptr);
+}
+
+TEST_F(AppControllerTest, DebounceTimerWorks) {
+ QSignalSpy successSpy(controller, &AppController::parseSuccess);
+
+ CodeEditor* innerEditor = textEditor->findChild<CodeEditor*>();
+ ASSERT_NE(innerEditor, nullptr);
+ 
+ // Type something
+ innerEditor->setPlainText("OPENQASM 3.0;\nqreg q[1];");
+
+ // The signal textChanged triggers debounce timer (400ms).
+ // It should not parse instantly.
+ EXPECT_EQ(successSpy.count(), 0);
+
+ // Wait slightly more than 400ms
+ QTest::qWait(450);
+
+ // Now it should have parsed
+ EXPECT_EQ(successSpy.count(), 1);
+}
