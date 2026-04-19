5453fc931def75e338acb4226d92f20e4b802ba5
Szymon <sz.oleskiewicz@gmail.com>
2026-04-19 13:51:19 +0200

    revert(qt-app): revert test change
    Signed-off-by: Szymon Oleśkiewicz <sz.oleskiewicz@gmail.com>

--- tests/gui/test_main_window.cpp
+++ tests/gui/test_main_window.cpp
@@ -23,7 +23,10 @@ class MainWindowTest : public ::testing::Test {
 };
 
 TEST_F(MainWindowTest, Initialization) {
-  EXPECT_EQ(mainWindow->windowTitle(), QString("QDE"));
+  EXPECT_EQ(mainWindow->windowTitle(), QString("QDE *"));
+  // when opening app an example source code is put inside which marks document
+  // as 'modified', hence the ' *' is added to the windowTitle
+  // NOTE: if this behavior is changed in the future this test will fail
 
   auto* splitter = mainWindow->findChild<QSplitter*>();
   ASSERT_NE(splitter, nullptr);
