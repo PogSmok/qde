7bf90754e8adec1b75061d7a785678f06d894444
PogSmok <lukaszprus.job@gmail.com>
2026-05-03 21:08:35 +0200

    feat(parser): create dummy qubit allocator

---
+++ include/qde/qubit_allocator.hpp
@@ -0,0 +1,20 @@
+#ifndef QUBIT_ALLOCATOR_HPP_
+#define QUBIT_ALLOCATOR_HPP_
+
+#include <string>
+#include <unordered_map>
+
+#include "qde/circuit.hpp"
+
+namespace qde {
+
+class QubitAllocator {
+ public:
+  static std::unordered_map<std::string, std::size_t>
+      allocate(const Circuit& circuit);
+
+};
+
+}  // namespace qde
+
+#endif  // QUBIT_ALLOCATOR_HPP_
---
+++ src/qubit_allocator.cpp
@@ -0,0 +1,30 @@
+#include "qde/qubit_allocator.hpp"
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "qde/circuit.hpp"
+
+namespace qde {
+
+// TODO allocate against actual constrained hardware config
+// this will also change function signature to also take const T& config
+std::unordered_map<std::string, std::string>  qde::QubitAllocator::allocate(
+    const Circuit& circuit) {
+  std::unordered_map<std::string, std::size_t> mapping;
+
+  const std::vector<QubitRegister>& qregs = circuit.qubitRegisters();
+  std::size_t id = 0;
+
+  for(const QubitRegister& qreg: qregs) {
+    for(std::size_t i = 0; i < qreg.size; i++) {
+      mapping[qreg.name + "[" + std::to_string(i) + "]"] = id;
+      id++;
+    }
+  }
+
+  return mapping;
+}
+
+}  // namespace qde
